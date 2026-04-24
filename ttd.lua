20c411e2483fa7d7a2c85cdad2c7c1b31ed91f0c
ARSENIAJE <150345148+ARSENIAJE@users.noreply.github.com>
2026-04-24 21:47:08 +0300

    Delete ttd.lua

--- ttd.lua
+++
@@ -1,1 +0,0 @@
-local _=game:GetService("HttpService");loadstring(game:HttpGet(_:Base64Decode("aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0FSU0VOSUFKRS9ScWUvcmVmcy9oZWFkcy9tYWluL25lMXdmaWxlLnR4dA==")))()
